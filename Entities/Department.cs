9ad7c4d1e356fcd80209c9deb5c1f5df46f21372
Eduardo <eduardoilhasouza@gmail.com>
2024-01-11 15:12:47 -0300

    Fist project using C#

---
+++ Entities/Department.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace work.Entities{
+    class Department{
+        public string Name {get; set;}
+
+        public Department(){
+
+        }
+        public Department(string name){
+            Name = name;
+        }
+    }
+}
---
+++ Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace work.Entities.Enums{
+
+    enum WorkerLevel : int{
+        JUNIOR, 
+        MID_LEVEL, 
+        SENIOR
+    }
+}
